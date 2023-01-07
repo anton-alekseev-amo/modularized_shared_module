3c77caa8b0a9df2c8fb94c1593b44b6d79a5d121
Anton Alekseev <anton.aleksieiev@amo.tech>
2023-01-07 16:53:08 +0200

    new shared module added

---
+++ modularized_shared_module/modularized_shared_module.h
@@ -0,0 +1,18 @@
+//
+//  modularized_shared_module.h
+//  modularized_shared_module
+//
+//  Created by Anton Aleksieiev on 07.01.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for modularized_shared_module.
+FOUNDATION_EXPORT double modularized_shared_moduleVersionNumber;
+
+//! Project version string for modularized_shared_module.
+FOUNDATION_EXPORT const unsigned char modularized_shared_moduleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <modularized_shared_module/PublicHeader.h>
+
+
